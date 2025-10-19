7c3db591efe94dd6f58f0c1fb438934f3c207dab
hythamalmarie-stack <hytham.almarie@gmail.com>
2025-10-19 16:45:10 +0300

    Hello

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import streamlit as st
 
 api_key = st.secrets['OPENAI_API_KEY']
 from langchain_openai import ChatOpenAI
-from langchain.schema import HumanMessage, AIMessage, SystemMessage
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+
 
 st.markdown("# ChatGPT-like clone")
 
